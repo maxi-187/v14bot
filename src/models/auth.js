87828e52c2c5a62e622900282c4eb36365b77b6b
maxi-187 <147093113+maxi-187@users.noreply.github.com>
2023-11-28 01:54:15 +0100

    Delete src directory

--- src/models/auth.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-let authSchema = new Schema({
-    GuildID: String,
-    ChannelID: String,
-    RoleID: String,
-});
-
-module.exports = model('authSchema', authSchema)
