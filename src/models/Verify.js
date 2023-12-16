25032248e4f5c9004913b2e779b8ad0cdf40a49a
maxi-187 <147093113+maxi-187@users.noreply.github.com>
2023-12-16 23:15:33 +0100

    Delete src directory

--- src/models/Verify.js
+++
@@ -1,8 +0,0 @@
-const {model, Schema} = require('mongoose');
-
-let VerifySchema = new Schema({
-    GuildID: String,
-    ChannelID: String,
-});
-
-module.exports = model('Verify', VerifySchema); 
--- src/models/maintenance.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const maintenanceSchema = new Schema({
-    maintenanceMode: { type: Boolean, default: false },
-});
-
-module.exports = model('maintenance', maintenanceSchema);
