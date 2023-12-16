a3dae6cb83a54c556b38215864ccc499c1a3732c
maxi-187 <147093113+maxi-187@users.noreply.github.com>
2023-12-16 23:44:35 +0100

    Delete src directory

--- src/models/BadWord.js
+++
@@ -1,7 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let BadWordSchema = new Schema({
-  word: { type: String, required: true },
-});
-
-module.exports = model("BadWord", BadWordSchema);
